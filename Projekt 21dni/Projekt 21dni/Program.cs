cfc43af84c67d30ea738dd912280a2d2bf9436a2
Kamil Burzyński <212kamilburzynski@gmail.com>
2023-05-13 22:59:29 +0200

    Dzień 4 rozwiązanie zadania.

--- Projekt 21dni/Projekt 21dni/Program.cs
+++ Projekt 21dni/Projekt 21dni/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+int age = 29;
+var gender = "kobieta";
+
+if (gender == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej lat 30");
+}
+    else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+    if (gender == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("niepełnooletni mężczyzna");
+}
